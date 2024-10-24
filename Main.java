608739ef430572d6049809d9ba860a8ef2ffe071
Gseyal <gautamseyal2020@gmail.com>
2024-10-24 13:45:16 +0530

    Adder added

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main{
     public static void main(String [] args){
-        System.out.println("Welcome to the System");
+        System.out.println("Welcome to the Adder Brached Changes");
     }
 }
